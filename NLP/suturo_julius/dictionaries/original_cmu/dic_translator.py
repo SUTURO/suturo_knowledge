fe6518cc285a849f0bf6d14affc3819548e7d02c
Paul Schnipper <schnippa@uni-bremen.de>
2020-01-27 16:04:04 +0100

    create scripts to split and to randomly minimize dic

--- NLP/suturo_julius/dictionaries/original_cmu/dic_translator.py
+++ NLP/suturo_julius/dictionaries/original_cmu/dic_translator.py
@@ -1,5 +1,5 @@
 if __name__ == "__main__":
-    source = "CMU_Sphinx_dic.txt"
+    source = "CMU_Sphinx_dic_original.txt"
     target = "CMU_Sphinx_dic_edit.txt"
 
     s = open(source, mode="r")
@@ -10,14 +10,10 @@ if __name__ == "__main__":
     log = open("log.txt", mode="w")
     log.flush()
     for line in content:
-        if line.__contains__("(2)"): # TODO regex
+        if not (line.__contains__("(") or line.__contains__("!") or line.__contains__(")") or line.__contains__("ey z")): # TODO add more
             lineSplit = line.split("\t")
-            lineSplit[1] = lineSplit[1].lower()
-            lineSplit[0] = lineSplit[0][0:-3]
-            t.write(lineSplit[0] + "\t" + lineSplit[1])
-        if not (line.__contains__("(") or line.__contains__("!") or line.__contains__(")")): # TODO add more
-            lineSplit = line.split("\t")
-            lineSplit[1] = lineSplit[1].lower()
-            t.write(lineSplit[0] + "\t" + lineSplit[1])
+            if len(lineSplit[0]) < 8:
+                lineSplit[1] = lineSplit[1].lower()
+                t.write(lineSplit[0] + "\t" + lineSplit[1])
         else:
-            log.write(line) # I write cut lines to this file
+            log.write(line)  # I write cut lines to this file
