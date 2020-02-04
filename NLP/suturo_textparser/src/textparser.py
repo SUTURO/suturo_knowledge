9199f79851d38277575d92875e73d1a2d105d962
Paul Schnipper <schnippa@uni-bremen.de>
2020-02-04 14:51:21 +0100

    fix up textparser

--- NLP/suturo_textparser/src/textparser.py
+++ NLP/suturo_textparser/src/textparser.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 import rospy
-from tmc_rosjulius_msgs.msg import RecogitionResult
+from tmc_rosjulius_msgs.msg import RecognitionResult
+from nlp_msgs.msg import StaticCommand
 
 
 def callback(data):
     sentence = findFirst(data)
-    if not sentence == "" and isStatic(sentence):
-
+    # if not sentence == "" and not isStatic(sentence):
+    isStatic(sentence)
+    # TODO make sling call
 
 
 
 def isStatic(sentence):
     if isStop(sentence):
-        # TODO do the stopping
+        command = StaticCommand()
+        command.command = StaticCommand.STOP
+        pub.publish(command)
         return True
     elif isStart(sentence):
-        # TODO do the starting
+        command = StaticCommand()
+        command.command = StaticCommand.START
+        pub.publish(command)
         return True
     return False
 
@@ -23,8 +29,10 @@ def isStatic(sentence):
 def isStop(sentence):
     return isInFile(sentence, '../commands/stop.txt')
 
+
 def isStart(sentence):
     return isInFile(sentence, '../commands/start.txt')
+
 
 def isInFile(sentence, filepath):
     with open(filepath) as f:
@@ -35,24 +43,22 @@ def isInFile(sentence, filepath):
 
 
 def findFirst(data):
-    mostliklySentence = data.sentence[0]
+    mostliklySentence = data.sentences[0]
     score1 = data.scores[0]
-    score2 = data.scores[1]
 
-    if data.scores.length == 1 and score1 > 0.80:
+    if len(data.scores) == 1 and score1 > 0.80:
         return mostliklySentence
+    score2 = data.scores[1]
     if score1 > 0.93 and score1 - score2 > 0.1:
         return mostliklySentence
     return ""
 
 
-
-def listener():
-    rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("input", RecognitionResult, callback)
-    rospy.spin()
-
-
 if __name__ == '__main__':
-    listener()
+    try:
+        rospy.init_node('textparser', anonymous=False)
+        pub = rospy.Publisher('hard_commands', StaticCommand, queue_size=10)
+        rospy.Subscriber('input', RecognitionResult, callback)
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
