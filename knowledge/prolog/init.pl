8a64bb9a85d228130851f239f421903580a6dcad
Paul Schnipper <schnippa@uni-bremen.de>
2020-02-14 00:14:25 +0100

    add urdf loading

--- knowledge/prolog/init.pl
+++ knowledge/prolog/init.pl
@@ -5,8 +5,16 @@
 :- register_ros_package(srdl).
 :- register_ros_package(knowrob_vis).
 :- register_ros_package(knowrob_memory).
+:- register_ros_package(urdfprolog).
 
-:- rdf_db:rdf_register_ns(hsr_objects, 'http://www.semanticweb.org/suturo/ontologies/2018/10/objects#', [keep(true)]).
+
+%:- rdf_db:rdf_register_ns(hsr_objects, 'http://www.semanticweb.org/suturo/ontologies/2018/10/objects#', [keep(true)]).
+
+:- ros_package_path('knowledge', X),
+    atom_concat(X, '/urdf/hsrb_lab.urdf', FileURL),
+    kb_create(urdf:'Robot', Robot),
+    rdf_urdf_load(Robot, FileURL).
+
 :- rdf_db:rdf_register_ns(robocup, 'http://knowrob.org/kb/robocup.owl#', [keep(true)]).
 :- rdf_db:rdf_register_ns(srdl2_comp, 'http://knowrob.org/kb/srdl2-comp.owl#', [keep(true)]).
 
