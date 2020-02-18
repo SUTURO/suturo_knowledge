450fc88cfd6b76b409abd11d19bc2db657bf4aa9
Paul Schnipper <schnippa@uni-bremen.de>
2020-02-18 22:38:32 +0100

    do stuff

--- knowledge/prolog/init.pl
+++ knowledge/prolog/init.pl
@@ -7,37 +7,36 @@
 :- register_ros_package(knowrob_memory).
 :- register_ros_package(urdfprolog).
 
-
-:- rdf_db:rdf_register_ns(hsr_objects, 'http://www.semanticweb.org/suturo/ontologies/2018/10/objects#', [keep(true)]).
-
-:- ros_package_path('knowledge', X),
-    atom_concat(X, '/urdf/hsrb_lab.urdf', FileURL),
-    kb_create(urdf:'Robot', Robot),
-    rdf_urdf_load(Robot, FileURL).
-
-
-%:- rdf_db:rdf_register_ns(robocup, 'http://knowrob.org/kb/robocup.owl#', [keep(true)]).
-:- rdf_db:rdf_register_ns(srdl2_comp, 'http://knowrob.org/kb/srdl2-comp.owl#', [keep(true)]).
-
 :- use_module(library('semweb/rdf_db')).
 :- use_module(library('semweb/rdfs')).
 :- use_module(library('semweb/owl_parser')).
 :- use_module(library('semweb/owl')).
+:- use_module(library('knowrob/knowrob')).
 :- use_module(library('knowrob/computable')).
 :- use_module(library('knowrob/temporal')).
 :- use_module(library('knowrob/beliefstate')).
 :- use_module(library('knowrob/transforms')).
-%:- use_module(library('knowrob/owl')).
-%:- use_module(library('knowrob/knowrob/rdfs')).
 :- use_module(library('knowrob/vis')).
-%:- use_module(library('knowrob/comp_spatial')).
-%:- use_module(library('knowrob/owl_export')).
+
+:- use_module(library('rdf_urdf')).
+:- use_module(library('urdf_parser')).
 
 :- use_module(library('object_state')).
 :- use_module(library('surfaces')).
 :- use_module(library('beliefstate')).
 :- use_module(library('spatial_comp')).
-%:- use_module(library('static_preset')).
 
-:- owl_parse('package://knowrob_common/owl/knowrob.owl').
-:- owl_parse('package://knowledge/owl/objects.owl').
+:- rdf_db:rdf_register_ns(hsr_objects, 'http://www.semanticweb.org/suturo/ontologies/2018/10/objects#', [keep(true)]).
+:- rdf_db:rdf_register_ns(urdf, 'http://knowrob.org/kb/urdf.owl#', [keep(true)]).
+:- rdf_db:rdf_register_ns(srdl2_comp, 'http://knowrob.org/kb/srdl2-comp.owl#', [keep(true)]).
+
+
+:- owl_parser:owl_parse('package://knowrob_common/owl/knowrob.owl').
+:- owl_parser:owl_parse('package://knowledge/owl/objects.owl').
+
+:- owl_parser:owl_parse('package://urdfprolog/owl/urdf.owl'). %would be used if we got rdf_urdf to work
+
+:- ros_package_path('knowledge', X),
+    atom_concat(X, '/urdf/hsrb_lab.urdf', FileURL),
+    kb_create(urdf:'Robot', Robot),
+    rdf_urdf_load(Robot, FileURL).
