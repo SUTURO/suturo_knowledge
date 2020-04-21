5607cc52fe7095458168a31ea61bc6585a206ac7
JeremiasThun <thun@uni-bremen.de>
2020-04-21 13:17:13 +0200

    bugfix place object

--- knowledge/scripts/store_object_info_server.py
+++ knowledge/scripts/store_object_info_server.py
@@ -78,8 +78,7 @@ class StoreObjectInfoServer(object):
                                 "[" + ", ".join([qx, qy, qz, qw]) + "]]," +
                                 threshold + ", ObjectInstance," +
                                 "[" + ", ".join([depth, width, height]) + "], " + shape + ", _, " +
-                                "[" + ", ".join([r, g, b, a]) + "], " + confidence_color + ")," +
-                            "place_object(ObjectInstance).")
+                                "[" + ", ".join([r, g, b, a]) + "], " + confidence_color + ").")
             rospy.loginfo('Send query: \n' + query_string)
             solutions = prolog.all_solutions(query_string)
             if not solutions:
