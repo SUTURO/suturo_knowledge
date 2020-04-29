97ec2b84a1be4b05059ca309d6c433e8e424bee7
JeremiasThun <thun@uni-bremen.de>
2020-04-29 12:59:10 +0200

    fix next_object

--- knowledge/prolog/pickup.pl
+++ knowledge/prolog/pickup.pl
@@ -11,7 +11,7 @@
     
 
 next_object(BestObj) :-
-    ( not(place_objects) -> writeln("Not all Objects could have been placed") ),
+    not(place_objects),
     all_objects_on_source_surfaces(Objs),
     maplist(distance_to_robot, Objs, Distances),
     min_list(Distances, MinDistance),
@@ -34,4 +34,7 @@ next_object(noObjectsOnSourceSurfaces) :-
 
 place_objects :-
     hsr_existing_objects(Objs),
-    forall(member(Obj, Objs), place_object(Obj)).
+    forall(member(Obj, Objs), place_object(Obj)), !.
+
+place_objects :-
+    writeln("Not all objects could have been added.").
