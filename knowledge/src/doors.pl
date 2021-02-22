15076f7111e6fb3cc5ad16105752cdd6cdbabf82
Karen S <karenscholz@live.de>
2021-02-22 19:32:48 +0100

    return door state as integer

--- knowledge/src/doors.pl
+++ knowledge/src/doors.pl
@@ -9,7 +9,12 @@
 :- rdf_db:rdf_register_ns(hsr_rooms, 'http://www.semanticweb.org/suturo/ontologies/2021/0/rooms#', [keep(true)]).
 
 
-
+%% init_doors
+%  
+%  Creates an object instance of concept door
+%  for each door specified in the URDF
+%  in the knowledge base and initializes
+%  its opening angle to 0.0 (fully closed)
 init_doors :-
     get_urdf_id(URDF),
     urdf_link_names(URDF, Links),
@@ -27,6 +32,14 @@ init_doors :-
     )).
 
 
+%% update_door_state(Door, Angle)
+%
+%  Sets the opening angle of the specified
+%  door to the specified angle.
+%
+%  @param Door Name of the door of which the 
+%  opening angle should be updated as a string
+%  @param Angle New value of the opening angle as float
 update_door_state(Door, Angle) :-
     object_frame_name(Door, DoorName),
     get_urdf_id(URDF),
@@ -41,6 +54,14 @@ update_door_state(Door, Angle) :-
     ).
 
 
+%% get_door_state(Door, DoorState)
+%  
+%  returns the state of the specified door
+%
+%  @param Door Name of the door as a string
+%  @param DoorState Sate of the door i.e.:
+%  0 - closed
+%  1 - open
 get_door_state(Door, DoorState) :-
     object_frame_name(Door, DoorName),
     get_urdf_id(URDF),
@@ -49,11 +70,19 @@ get_door_state(Door, DoorState) :-
     min_door_joint_angle(MaxAngle),
     MinAngle is -1*MaxAngle,
     ( JointState < MaxAngle, JointState > MinAngle 
-    -> DoorState = 'closed'
-    ; DoorState = 'open'
+    -> DoorState = 0
+    ; DoorState = 1
     ).
 
 
+%% get_all_door_states(DoorStates)
+%
+%  returns a list of all doors and their states 
+%
+%  @param DoorStates List of doors and their
+%  corresponding state i.e.:
+%  0 - closed
+%  1 - open
 get_all_door_states(DoorStates) :-
     findall([Door, State], 
     (
@@ -64,6 +93,16 @@ get_all_door_states(DoorStates) :-
     ).
 
 
+
+%% is_valid_joint_state(DoorJoint, JointState)
+%  
+%  checks whether a joint movement is valid/ possible
+%  for the specified joint
+%
+%  @param DoorJoint Joint for which a movement should
+%  be checked
+%  @param JointState movement the specified joint should 
+%  perform e.g.: an angle for a revolute joint
 is_valid_joint_state(DoorJoint, JointState) :-
     get_urdf_id(URDF),
     urdf_joint_hard_limits(URDF, DoorJoint, [LowerLimit, UpperLimit], _, _),
@@ -73,5 +112,11 @@ is_valid_joint_state(DoorJoint, JointState) :-
     ).
 
 
+%% min_door_joint_angle(Angle)
+%
+%  returns the the minimum opening angle
+%  of a door to be seen as open
+%
+%  @param Angle Minimum opening angle
 min_door_joint_angle(Angle) :-
     Angle = 1.22. % corresponds to 70 degree
