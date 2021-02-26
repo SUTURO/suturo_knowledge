60dbc9f425cd3ca3899d74ea6d1591491db08ee4
Paul Schnipper <schnippa@uni-bremen.de>
2021-02-26 16:39:53 +0100

    translate surfaces v1

--- knowledge/prolog/nlg.pl
+++ knowledge/prolog/nlg.pl
@@ -19,9 +19,32 @@ object_tts(ObjID,Name):-
     ; ros_error("nlg.pl unable to deterimine name"), Name = ObjID
     ).
 
-%TODO
+
 surface_tts(SurID,Name):-
-    Name  = SurID.
+    % TODO what is SurID?
+    % like iai_kitchen/bookshelf_clone_0_floor_1_piece ?
+    % like http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#PhysicalObject_BWLUONCK ?
+    % or
+    % like bookshelf_clone_0_floor_1_piece // bed_table_center ?
+    % depending use urdf_frame(http://www.ontologydesignpatterns.org/ont/dul/DUL.owl#PhysicalObject_BWLUONCK , bookshelf_clone_0_floor_1_piece)
+    % this code expects bookshelf_clone_0_floor_1_piece
+
+    split_string(SurID,"_","",Split),
+    % Split e.g. ["bed", "table", "center"]
+    ignore(nth1(Index,Split,"table")),
+    ignore(nth1(Index,Split,"bookshelf")),
+    ignore(nth1(Index,Split,"shelf")),
+    ignore(nth1(Index,Split,"shefl")),
+    ignore(nth1(Index,Split,"bucket")),
+    (number(Index); % When we didnt get an Index
+    length(Split,Index)), % we use the max index
+    % Index e.g. 2
+    findall(E, (nth1(I,Split,E), I =< Index), NameSplit),
+    % NameSplit e.g. ["bed", "table"]
+    atomic_list_concat(NameSplit, " ", NameWoThe),
+    % NameWoThe e.g. bed table 
+    atomic_concat('the ', NameWoThe, Name).
+    % Name e.g. the bed table 
 
 %TODO
 room_tts(RoomID,Name):-
