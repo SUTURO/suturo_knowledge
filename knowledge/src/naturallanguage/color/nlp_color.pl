a0553e39cb141dc748a1441b0f80f4e886a4d9e2
Paul Schnipper <schnippa@uni-bremen.de>
2021-07-13 16:39:26 +0200

    add color to name

--- knowledge/src/naturallanguage/color/nlp_color.pl
+++ knowledge/src/naturallanguage/color/nlp_color.pl
@@ -1,7 +1,105 @@
 :- module(nlp_color,[
-    rgb_to_hsv/2]).
+    set_nlp_color/1]).
 
 rdf_meta.
 
 :- use_foreign_library('libnlp_color.so').
 
+:- ros_package_iri(knowledge, 'package://knowledge/owl/color.owl').
+
+:- tripledb_load(
+	'package://knowledge/owl/color.owl',
+	[ namespace(suturo_color, 'http://www.semanticweb.org/suturo/ontologies/2021/6/color#')
+	]).
+
+:- rdf_db:rdf_register_ns(suturo_color, 'http://www.semanticweb.org/suturo/ontologies/2021/6/color#', [keep(true)]).
+
+set_nlp_color(ObjID):-
+    object_color_rgb(ObjID, RGB),
+    rgb_to_hsv(RGB, HSV),
+    tell(object_color_hsv(ObjID,[H,S,V])),
+    determine_color_name(ObjID,Color),
+    tell(triple(ObjID, suturo_color:'has_color', Color)).
+
+
+
+
+
+
+determine_color_name(ObjID,Color):-
+    ask(object_color_hsv(ObjID,[ObjH,ObjS,ObjV])),
+    determine_color_name([_,ObjSat,ObjVal],Color).
+
+
+determine_color_name([_,ObjSat,ObjVal],Color):-
+    ObjSat < 0.3,
+    ObjVal < 0.6,
+    Color = 'http://www.semanticweb.org/suturo/ontologies/2021/6/color#grey',
+    !.
+
+determine_color_name([_,ObjSat,ObjVal],Color):-
+    ObjSat < 0.12,
+    Color = 'http://www.semanticweb.org/suturo/ontologies/2021/6/color#white',
+    !.
+
+determine_color_name([_,ObjSat,ObjVal],Color):-
+    ObjVal < 0.3,
+    Color = 'http://www.semanticweb.org/suturo/ontologies/2021/6/color#black',
+    !.
+
+determine_color_name([ObjH,_,_],Color):-
+
+    findall(ColorValueDistribution,
+        subclass_of(ColorValueDistribution, suturo_color:'hsv_values'),
+    ColorValueDistributions),
+
+    findall(Tuple,
+    (
+        member(CVD, ColorValueDistributions),
+        triple(CVD, suturo_color:'hue',Hue),
+        triple(CVD,suturo_color:'standard_deviation',SD),
+        triple(CVD,suturo_color:'multiplier',Multi),
+        normal_distribution_propability(Hue,SD,ObjH,Prop),
+        PropM is Prop * Multi,
+        Tuple = [PropM, CVD] % PropM first so max_member uses that
+    ),
+    Tuples),
+    max_member([_,ColorValue], Tuples),
+    triple(Color,suturo_color:'color_likelihood', ColorValue).
+
+
+
+%% object_color_hsv(?Obj, ?Col) is nondet.
+%
+% True if Col is the main color of Obj.
+% Col is encoded as as [float hue, saturation, value], hue 0-360, saturation,value of 0-1.
+%
+% @param Obj object resource
+% @param Col hsv color data
+% 
+object_color_hsv(Obj,[H,S,V]) ?>
+  holds(Obj,soma:hasColor,Color),
+  holds(Color,dul:hasRegion,Region),
+  holds(Region,soma:hasHSVValue,ColorValue),
+  { atomic_list_concat(ColorList, ',', ColorValue),
+    maplist(atom_number, ColorList, [H,S,V]) },
+  { ! }.
+
+object_color_hsv(Obj, [H,S,V]) ?>
+  holds(Obj,soma:hasHSVValue,ColorValue),
+  { atomic_list_concat(ColorList, ',', ColorValue),
+    maplist(atom_number, ColorList, [H,S,V]) },
+  { ! }.
+  
+object_color_hsv(Obj,[H,S,V]) +>
+  % get the color quality
+  { holds(Obj,soma:hasColor,Color),
+    atomic_list_concat([H,S,V],',', ColorValue)},
+  % create a new region
+  { universal_scope(US),
+    tell([ has_type(Region,soma:'ColorRegion'),
+           holds(Region,soma:hasHSVValue, ColorValue)
+         ],[[],US])
+  },
+  % update the region of the color quality
+  update(holds(Color,dul:hasRegion,Region)).
---
+++ knowledge/src/naturallanguage/nlp.pl
@@ -0,0 +1,101 @@
+:- module(nlp,
+    [
+    matching_object_in_room_name/3,
+    matching_object_in_room_id/3,
+    matching_object_in_furniture_id/3,
+    matching_object_everywhere/2,
+    all_obj_names/1,
+    all_obj_names_with_id/1,
+    matching_obj_classes/2,
+    matching_object_in_list/3,
+    matching_room_class/2,
+    matching_room/2,
+    all_room_names_with_id/1,
+    deliver_object_pose/2
+    ]).
+
+:- rdf_meta
+    all_obj_names(r),
+    all_obj_names_with_id(r),
+    matching_classes(r,r).
+
+
+matching_object_in_room_name(ObjectName, RoomName, Obj):-
+    matching_room(RoomName,RoomId),
+    matching_object_in_room_id(ObjectName, RoomId, Obj).
+
+
+matching_object_in_room_id(ObjectName, RoomId, Obj):-
+    objects_in_room(RoomId,Objects),
+    matching_object_in_list(ObjectName, Objects, Obj).
+
+
+matching_object_in_furniture_id(ObjectName, FurnitureId, Obj):-
+    objects_on_furniture(FurnitureId,Objects),
+    matching_object_in_list(ObjectName, Objects, Obj).
+
+
+
+matching_object_everywhere(Name,Obj):-
+    hsr_existing_objects(Objects),
+    matching_object_in_list(Name ,Objects, Obj).
+
+%%%%%OBJECTS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+all_obj_names(Names):-
+    all_obj_names_with_id(AllNames),findall(Name, member([_,Name],AllNames),Names).
+
+all_obj_names_with_id(AllNames):-
+    findall(Tuple,
+            (ask(triple(ID, 'http://www.semanticweb.org/suturo/ontologies/2020/3/objects#TtSName',TtSName)),atom_string(TtSName,TtSNameString),
+            string_lower(TtSNameString,TtSNameStringLower),
+            Tuple = [ID,TtSNameStringLower]),
+            TtsNames),
+    findall(Tuple,
+            (subclass_of(Nlong,'http://ias.cs.tum.edu/kb/knowrob.owl#EnduringThing-Localized'),split_string(Nlong, "#","",L), nth0(1,L,Name),string_lower(Name,NameLower), Tuple = [Nlong,NameLower])
+            ,Names), append(TtsNames,Names,AllNamesWithDup),sort(AllNamesWithDup, AllNames).
+
+
+matching_obj_classes(Name,[Class|Classes]):-
+    all_obj_names_with_id(AllNames), member([Class,Name], AllNames), findall(SubClass, subclass_of(SubClass,Class), Classes).
+
+matching_object_in_list(Name, Objects, Obj):-
+    matching_obj_classes(Name,Classes), member(Obj,Objects), member(Class, Classes), ask(is_a(Obj,Class)).
+
+
+%%%%%ROOMS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+all_room_names(Names):-
+    all_room_names_with_id(AllNames),findall(Name, member([_,Name],AllNames),Names).
+
+
+all_room_names_with_id(AllNames):-
+    findall(Tuple,
+            (ask(triple(ID, 'http://www.semanticweb.org/suturo/ontologies/2021/0/rooms#TtSName',TtSName)),atom_string(TtSName,TtSNameString),
+            string_lower(TtSNameString,TtSNameStringLower),
+            Tuple = [ID,TtSNameStringLower]),
+            TtsNames),
+    findall(Tuple,
+            (subclass_of(Nlong,'http://www.semanticweb.org/suturo/ontologies/2021/0/rooms#Room'),split_string(Nlong, "#","",L), nth0(1,L,Name),string_lower(Name,NameLower), Tuple = [Nlong,NameLower])
+            ,Names), append(TtsNames,Names,AllNamesWithDup),sort(AllNamesWithDup, AllNames).
+
+
+matching_room_class(Name,Class):-
+    all_room_names_with_id(AllNames), member([Class,Name], AllNames).
+
+matching_room(Name,Room):-    
+    matching_room_class(Name,Class), all_rooms_of_type(Class,Rooms), member(Room, Rooms).
+
+
+
+
+
+%%%% RC People to Pose%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+deliver_object_pose('left', [[0.18,2.8,0],[0, 0, 0.99518, -0.098072]]).
+deliver_object_pose("left", Pose):-
+    deliver_object_pose('left', Pose).
+deliver_object_pose('right', [[0.2131, 3.9596, 0],[0, 0, 0.99998, -0.0067647]]).
+deliver_object_pose("right", Pose).
+    deliver_object_pose('right', Pose).
