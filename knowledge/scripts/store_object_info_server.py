8716d565d5550db6f0cf9e38fc2226759c4ff8b7
Tede von Knorre <tede@uni-bremen.de>
2021-11-26 13:26:28 +0100

    Replace tabs with spaces in store_object_info_server.py

--- knowledge/scripts/store_object_info_server.py
+++ knowledge/scripts/store_object_info_server.py
@@ -28,8 +28,8 @@ class StoreObjectInfoServer(object):
 
         success = True
 
-	if goal.detectionData == []:
-	    rospy.loginfo('detectionData was empty')
+        if goal.detectionData == []:
+            rospy.loginfo('detectionData was empty')
 
         for data in goal.detectionData:
 
@@ -82,12 +82,12 @@ class StoreObjectInfoServer(object):
             # (1) is_legal_obj_position
             # (2) create_object_at()
             query_string = ("is_legal_obj_position([" + ", ".join([x, y, z]) + "]),"+
-				"create_object('http://www.semanticweb.org/suturo/ontologies/2020/3/objects#" + obj_class + "'," + confidence_class + "," +
-				"['" + source_frame + "',[" + ", ".join([x, y, z]) + "],[" + ", ".join([qx, qy, qz, qw]) + "]]," +
-				"[" + ", ".join([depth, width, height]) + "], " +
-				"'box'," + "1" + # TODO shape conf is allways 1
-				",[" + ", ".join([r, g, b]) + "], " + confidence_color +
-				", ObjID).")
+                                "create_object('http://www.semanticweb.org/suturo/ontologies/2020/3/objects#" + obj_class + "'," + confidence_class + "," +
+                                "['" + source_frame + "',[" + ", ".join([x, y, z]) + "],[" + ", ".join([qx, qy, qz, qw]) + "]]," +
+                                "[" + ", ".join([depth, width, height]) + "], " +
+                                "'box'," + "1" + # TODO shape conf is allways 1
+                                ",[" + ", ".join([r, g, b]) + "], " + confidence_color +
+                                ", ObjID).")
 
             rospy.loginfo('Send query: \n' + query_string)
             solutions = prolog.all_solutions(query_string)
