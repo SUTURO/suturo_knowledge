3530d32b2e7441aa1648262d3a1e2c05eea2c62a
McModknower <teetoll@t-online.de>
2022-12-08 11:14:17 +0100

    Store table Size

--- suturo_knowledge/src/model/furniture_creation.pl
+++ suturo_knowledge/src/model/furniture_creation.pl
@@ -4,14 +4,22 @@
 	      create_table/2
 	  ]).
 
+:- use_module(furniture_types,
+	      [
+		  is_type/2,
+		  is_table/1
+	      ]).
+
 create_table(Table, [Depth, Width, Height]) :-
     kb_project(is_table(Table)),
     kb_project(is_shape(Shape)),
     kb_project(is_boxShape(ShapeRegion)),
     kb_project(holds(Table, soma:hasShape, Shape)),
-    kb_project(holds(Shape, dul:hasRegion, ShapeRegion)).
-    % FIXME currently throws an exception
-    %kb_project(object_dimensions(Table, Depth, Width, Height)).
+    kb_project(holds(Shape, dul:hasRegion, ShapeRegion)),
+    % Doesn't use object_dimensions/4 because it throws an exception
+    kb_project(holds(ShapeRegion, soma:hasDepth, Depth)),
+    kb_project(holds(ShapeRegion, soma:hasWidth, Width)),
+    kb_project(holds(ShapeRegion, soma:hasHeight, Height)).
 
 is_shape(Shape) ?+>
     is_type(Shape, soma:'Shape').
