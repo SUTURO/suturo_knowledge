8296c37c2a2503bb30826fa440406a854a70d020
md <info@malte3d.com>
2022-12-15 15:12:48 +0100

    Fix: deg_to_rad

--- suturo_knowledge/src/util/math.pl
+++ suturo_knowledge/src/util/math.pl
@@ -5,14 +5,14 @@
 	  ]).
 
 
-%% degree_to_radians(+Degrees, -Radians) is det
+%% deg_to_rad(+Degrees, -Radians) is det
 %
 % Converts Degrees to Radians
 %
 % @param Degrees The number of degrees to convert
 % @param Radians The number of radians
 %
-deg_to_rad(+Degrees, -Radians) :-
+deg_to_rad(Degrees, Radians) :-
     % Define the conversion factor from degrees to radians (pi/180)
     DegreesToRadiansConversionFactor = 0.0174533,
     Radians is Degrees * DegreesToRadiansConversionFactor.
