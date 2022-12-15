b8cfd6d7a115cecc3fecf0946f1811d0c7fab753
McModknower <teetoll@t-online.de>
2022-12-15 16:31:43 +0100

    Move object_pose/2 into object_info.pl

--- suturo_knowledge/src/model/object/__init__.pl
+++ suturo_knowledge/src/model/object/__init__.pl
@@ -1,7 +1,6 @@
 % This folder contains everything related to physical objects
 
 :- ensure_loaded('types').
-:- ensure_loaded('pose').
 
 :- ensure_loaded('object_creation').
 :- ensure_loaded('object_info').
--- suturo_knowledge/src/model/object/object_info.pl
+++ suturo_knowledge/src/model/object/object_info.pl
@@ -1,8 +1,19 @@
 %% The object info module contains predicates that provide information about the objects and their role in the world.
 :- module(object_info,
 	  [
-        object_rel_pose/3
+        object_rel_pose/3,
+        object_pose/2
 	  ]).
+
+:- use_module(library('ros/tf/tf'),
+	      % actually uses tf:tf_get_pose, but that is not exported by tf
+	      []).
+
+%% object_pose(+Object, -PoseStamped) is semidet.
+%
+% Get the pose of an object.
+object_pose(Object, PoseStamped) :-
+    tf:tf_get_pose(Object, PoseStamped).
 
 %% object_rel_pose(+Object, +Type, -PoseStamped) is semidet.
 %
