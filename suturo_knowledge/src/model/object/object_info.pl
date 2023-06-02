bcd45fbb6a92a18909ec79cb87f2514be9e6ba04
md <info@malte3d.com>
2023-06-02 14:53:54 +0200

    Feat: Preparations for predefined object locations

--- suturo_knowledge/src/model/object/object_info.pl
+++ suturo_knowledge/src/model/object/object_info.pl
@@ -1,7 +1,8 @@
 %% The object info module contains predicates that provide information about the objects and their role in the world.
 :- module(object_info,
 	  [
-      	object_pose(r,-)
+      	object_pose(r,-),
+      	object_predefined_destination_location(r,-)
 	  ]).
 
 :- use_module(library('ros/tf/tf'),
@@ -13,3 +14,23 @@
 % Get the pose of an object.
 object_pose(Object, PoseStamped) :-
     tf:tf_get_pose(Object, PoseStamped).
+
+%% object_predefined_origin_location(+Object, -PoseStamped) is semidet.
+%
+% Get the predefined origin location of an object.
+% 
+% @param Object The object.
+% @param PoseStamped The pose of the predefined origin location.
+%
+object_predefined_origin_location(Object, PoseStamped) :-
+    false.
+
+%% object_predefined_destination_location(+Object, -PoseStamped) is semidet.
+%
+% Get the predefined destination location of an object.
+% 
+% @param Object The object.
+% @param PoseStamped The pose of the predefined destination location.
+%
+object_predefined_destination_location(Object, PoseStamped) :-
+    false.
