ae53cfbd0ddc2542c2cc3e4daa73160a3aff8e04
md <info@malte3d.com>
2023-06-07 13:06:09 +0200

    Fix: Merge conflict

--- suturo_knowledge/src/model/object/object_info.pl
+++ suturo_knowledge/src/model/object/object_info.pl
@@ -21,3 +21,64 @@
 % Get the pose of an object.
 object_pose(Object, PoseStamped) :-
     tf:tf_get_pose(Object, PoseStamped).
+
+
+
+tiny_object(Object) :-
+	object_shape_workaround(Object,_,ShapeTerm,_,_),
+(
+	ShapeTerm = box(X,Y,Z),
+	(
+		X =< 0.05;
+		Y =< 0.05;
+		Z =< 0.05
+	);
+	ShapeTerm = cylinder(Radius,Length),
+	(
+		Radius =< 0.025;
+		Length =< 0.05
+	);
+	ShapeTerm = sphere(Radius),
+	(
+		Radius =< 0.025
+	);
+	ShapeTerm = mesh(File, Scale),
+	(
+		false
+	)
+).
+
+is_suturo_object(Object):-
+	is_physical_object(Object),
+	triple(Object, suturo:'hasDataSource', DataSource).
+
+set_object_handled(Object) :-
+	update_handle_state(Object, true).
+	
+set_object_not_handled(Object) :-
+	update_handle_state(Object, false).
+
+update_handle_state(Object, State) :-
+	is_suturo_object(Object),
+	triple(Object, suturo:'hasHandleState', HandleState),
+	forall(triple(HandleState, suturo:'handled', OldValue),
+		kb_unproject(triple(HandleState, suturo:'handled', OldValue))),
+    kb_project(triple(HandleState, suturo:'handled', State)).
+
+handled(Object) :-
+	is_suturo_object(Object),
+	triple(Object, suturo:'hasHandleState', HandleState),
+	triple(HandleState, suturo:'handled', true).
+
+not_handled(Object) :-
+	is_suturo_object(Object),
+	triple(Object, suturo:'hasHandleState', HandleState),
+	triple(HandleState, suturo:'handled', false).
+
+objects_not_handled(Objects):-
+    findall(Object,
+    (
+        is_suturo_object(Object),
+        not_handled(Object)
+    ),
+    Objects).
