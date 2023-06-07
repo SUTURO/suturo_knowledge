3a061fce7022b8e2d5bfab5575630a1dde1660a2
md <info@malte3d.com>
2023-06-07 13:14:15 +0200

    Doc: PlDoc for object info

--- suturo_knowledge/src/model/object/object_info.pl
+++ suturo_knowledge/src/model/object/object_info.pl
@@ -2,14 +2,14 @@
 :- module(object_info,
 	  [
       	object_pose(r,-),
-		tiny_object/1,
-		is_suturo_object/1,
-		set_object_handled/1,
-		set_object_not_handled/1,
-		update_handle_state/2,
-		handled/1,
-		not_handled/1,
-		objects_not_handled/1
+		tiny_object(r),
+		is_suturo_object(r),
+		set_object_handled(r),
+		set_object_not_handled(r),
+		update_handle_state(r,+),
+		handled(r),
+		not_handled(r),
+		objects_not_handled(-)
 	  ]).
 
 :- use_module(library('ros/tf/tf'),
@@ -22,8 +22,13 @@
 object_pose(Object, PoseStamped) :-
     tf:tf_get_pose(Object, PoseStamped).
 
-
-
+%% tiny_object(+Object) is semidet.
+%
+% True if the object is tiny according to the RoboCup rulebook.
+% The RoboCup rulebook states that objects with any side smaller than 5cm are considered tiny.
+%
+% @param Object The object to check.
+%
 tiny_object(Object) :-
 	object_shape_workaround(Object,_,ShapeTerm,_,_),
 (
@@ -48,16 +53,42 @@ tiny_object(Object) :-
 	)
 ).
 
+%% is_suturo_object(+Object) is semidet.
+%
+% True if the object is a Suturo object.
+% An object is a Suturo object if its a physical object and has a data source. 
+%
+% @param Object The object to check.
+%
 is_suturo_object(Object):-
 	is_physical_object(Object),
 	triple(Object, suturo:'hasDataSource', DataSource).
 
+%% is_physical_object(+Object) is det.
+%
+% Sets the state of the object to handled.
+%
+% @param Object The object to set the state of.
+%
 set_object_handled(Object) :-
 	update_handle_state(Object, true).
 	
+%% is_physical_object(+Object) is det.
+%
+% Sets the state of the object to not handled.
+%
+% @param Object The object to set the state of.
+%
 set_object_not_handled(Object) :-
 	update_handle_state(Object, false).
 
+%% update_handle_state(+Object, +State) is det.
+%
+% Updates the handle state of an object.
+%
+% @param Object The object to update the handle state of.
+% @param State The new state of the object. (true or false)
+%
 update_handle_state(Object, State) :-
 	is_suturo_object(Object),
 	triple(Object, suturo:'hasHandleState', HandleState),
@@ -65,16 +96,34 @@ update_handle_state(Object, State) :-
 		kb_unproject(triple(HandleState, suturo:'handled', OldValue))),
     kb_project(triple(HandleState, suturo:'handled', State)).
 
+%% handled(+Object) is semidet.
+%
+% True if the object is handled.
+%
+% @param Object The object to check.
+%
 handled(Object) :-
 	is_suturo_object(Object),
 	triple(Object, suturo:'hasHandleState', HandleState),
 	triple(HandleState, suturo:'handled', true).
 
+%% not_handled(+Object) is semidet.
+%
+% True if the object is not handled.
+%
+% @param Object The object to check.
+%
 not_handled(Object) :-
 	is_suturo_object(Object),
 	triple(Object, suturo:'hasHandleState', HandleState),
 	triple(HandleState, suturo:'handled', false).
 
+%% objects_not_handled(-Objects) is det.
+%
+% Returns a list of all objects that are not handled.
+%
+% @param Objects The list of objects that are not handled.
+%
 objects_not_handled(Objects):-
     findall(Object,
     (
