bf200bb879c1ccb751103828e56ee64c07c51c56
McModknower <teetoll@t-online.de>
2023-06-22 17:21:59 +0200

    reset_user_data alsow clears tf data

--- suturo_knowledge/src/util/reset.pl
+++ suturo_knowledge/src/util/reset.pl
@@ -4,8 +4,11 @@
 
 %% reset_user_data is det.
 %
-% drop the user graph (containing objects and data assigned at runtime)
+% drop the user graph (containing objects and data assigned at runtime),
+% drop all tf data,
 % and reinitialize the furnitures from the semantic map.
 reset_user_data :-
-	drop_graph(user),
-	init_furnitures.
+    drop_graph(user),
+    tf_mng_drop,
+    tf_mem_clear,
+    init_furnitures.
