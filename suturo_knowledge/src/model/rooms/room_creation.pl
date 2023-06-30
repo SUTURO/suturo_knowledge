f69e064be373d6d8b711e6ec8fec223a7cbde93a
McModknower <teetoll@t-online.de>
2023-06-30 10:40:12 +0200

    init entry and exit to rooms

--- suturo_knowledge/src/model/rooms/room_creation.pl
+++ suturo_knowledge/src/model/rooms/room_creation.pl
@@ -27,18 +27,21 @@ create_room(RoomType, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Depth, Width, IRI) :-
 create_room_entry(Room, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Entry) :-
     kb_project((is_type(Entry,soma:'Location'),
                 is_entry_to(Entry, Room))),
-    tf_set_pose(Entry, [Frame, [X,Y,Z], [RX,RY,RZ,RW]]).
+    from_current_scope(Scope),
+    tf_set_pose(Entry, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Scope).
 
 create_room_exit(Room, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Exit) :-
     kb_project((is_type(Exit,soma:'Location'),
                 is_exit_from(Exit, Room))),
-    tf_set_pose(Exit, [Frame, [X,Y,Z], [RX,RY,RZ,RW]]).
+    from_current_scope(Scope),
+    tf_set_pose(Exit, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Scope).
 
 create_room_entry_exit(Room, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Entry) :-
     kb_project((is_type(Entry,soma:'Location'),
                 is_exit_from(Entry, Room),
                 is_entry_to(Entry, Room))),
-    tf_set_pose(Entry, [Frame, [X,Y,Z], [RX,RY,RZ,RW]]).
+    from_current_scope(Scope),
+    tf_set_pose(Entry, [Frame, [X,Y,Z], [RX,RY,RZ,RW]], Scope).
 
 % see furniture_creation:init_furnitures for reference
 init_rooms :-
@@ -60,7 +63,8 @@ init_room_0(UrdfLink, RoomType) :-
     get_urdf_id(URDF),
     urdf_link_collision_shape(URDF, UrdfLink, box(DX,DY,_), _),
     create_room(RoomType, Pose, DX, DY, Room),
-    kb_project(has_urdf_name(Room, UrdfLink)).
+    kb_project(has_urdf_name(Room, UrdfLink)),
+    init_connections(URDF,UrdfLink,Room).
 
 room_type(UrdfLink, Type) :-
     atomic_list_concat([_,MapType,room_center_link], ':', UrdfLink),
@@ -72,3 +76,22 @@ room_map_type(kitchen, soma:'Kitchen') :- !.
 room_map_type(living_room, suturo:'LivingRoom') :- !.
 room_map_type(bedroom, suturo:'Bedroom') :- !.
 %% TODO: add dynamic room type support
+
+init_connections(URDF,UrdfLink,Room) :-
+    urdf_link_child_joints(URDF,UrdfLink,Joints),
+    forall((member(Joint,Joints),
+            urdf_joint_child_link(URDF,Joint,Child)),
+           log_result(init_connection_link(Room,Child), 'Created room connection ~w', [Child])).
+
+init_connection_link(Room,UrdfLink) :-
+    furniture_creation:furniture_pose(UrdfLink, Pose),
+    atomic_list_concat([_,MapType,_], ':', UrdfLink),
+    init_connection_link0(MapType, Room, Pose, Location),
+    kb_project(has_urdf_name(Location, UrdfLink)).
+
+init_connection_link0(entry, Room, Pose, Location) :-
+    create_room_entry(Room, Pose, Location).
+init_connection_link0(exit, Room, Pose, Location) :-
+    create_room_exit(Room, Pose, Location).
+init_connection_link0(entry_exit, Room, Pose, Location) :-
+    create_room_entry_exit(Room, Pose, Location).
