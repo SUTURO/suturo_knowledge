86876f93f13f6828d40ad893d932800203aa9c46
KonschinA <alina.konschin@gmail.com>
2024-04-26 12:13:40 +0200

    - added for testing the property hasOriginLocation - nav poses calculation

--- suturo_knowledge/src/model/object/artifact/furniture/furniture_info.pl
+++ suturo_knowledge/src/model/object/artifact/furniture/furniture_info.pl
@@ -2,7 +2,11 @@
 :- module(furniture_info,
 	[
 		furniture_rel_pose(r,+,-),
-        has_robocup_name(r,?)
+        has_robocup_name(r,?),
+		div_parts(+, -),
+		div_partss(+,-),
+		divide_next(+,+,-),
+		opt_nav_poses(+, -)
 	]).
 
 :- use_module(library('util/math'),
@@ -34,7 +38,51 @@ furniture_rel_pose_perceive(Furniture, PoseStamped) :-
 	% Get the PoseStamped of the Furniture
 	object_pose(Furniture, [Frame, [X,Y,Z], Rotation]),
 	XNew is X - 0.7,
+	opt_nav_poses(Furiniture, Middle),
     PoseStamped = [Frame, [XNew,Y,Z], Rotation].
+
+% optional navigation poses when interacting with furniture items
+% Middle is the new x position
+%% opt_nav_poses
+opt_nav_poses(Furniture, Size):-
+	object_shape_workaround(Furniture, _, ShapeTerm,_,_),
+	dir_size('-y', ShapeTerm, Size).
+	%try_divide(Size, Middle).
+		
+try_divide(Size, Middle) :-
+	(   div_parts(Size, Middle)
+	;   div_partss(Size, Middle)  
+	).
+		
+	div_parts(Size, Middle) :-
+		Size > 0, 
+		Size =< 0.8,
+		Middle is Size / 2.0.
+		
+	div_partss(Size, Midpoints):-
+		Size > 0.8,
+		writeln("1."),
+		Middle is 0.4,
+		writeln("2."),
+		divide_next(Size, 0.8, [Middle]). % add 40 in list
+
+	divide_next(Size, Save, Midpoints):-
+		(	Size > Save,
+			writeln("3.")
+		->	NSave is Save + 0.7,
+			Middle is Save - (0.7/ 2.0),
+			divide_next(Size, NSave, Midpoints)
+		;	Midpoints = 0.7,
+			writeln("4. ~w", [Midpoints])
+		).
+
+
+	
+
+% divides the longest side of the furniture into parts 
+% parts have size of the plane that the robots camera can perceive w/ head rotation
+%% divide(+Size, -Middle)
+
 	% deg_to_rad(70, CameraViewAngle)
 
 	% TODO: Calculate perceiving position relative to the Furniture
