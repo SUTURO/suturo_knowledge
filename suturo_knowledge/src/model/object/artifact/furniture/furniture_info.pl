d2623823259ce7919f5b51c40874d39904669f70
KonschinA <alina.konschin@gmail.com>
2024-06-28 14:40:14 +0200

    - shortest_side

--- suturo_knowledge/src/model/object/artifact/furniture/furniture_info.pl
+++ suturo_knowledge/src/model/object/artifact/furniture/furniture_info.pl
@@ -111,14 +111,14 @@ longest_side(Furniture, Size):-
 	; Size = YSize
 	).
 
-	shortest_side(Furniture, Size):-
-		object_shape_workaround(Furniture, _, ShapeTerm, _, _),
-		dir_size('-x', ShapeTerm, XSize),
-		dir_size('-y', ShapeTerm, YSize),
-		( XSize < YSize 
-		-> Size = XSize
-		; Size = YSize
-		).
+shortest_side(Furniture, Size):-
+	object_shape_workaround(Furniture, _, ShapeTerm, _, _),
+	dir_size('-x', ShapeTerm, XSize),
+	dir_size('-y', ShapeTerm, YSize),
+	( XSize < YSize 
+	-> Size = XSize
+	; Size = YSize
+	).
 
 furniture_rel_pose_interact(Furniture, PoseStamped) :-
 	% Get the PoseStamped of the Furniture
